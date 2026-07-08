d293190bf0a61a63784a398581f1c97c50c3a28f
Bunty Saini <sainibunty2309@gmail.com>
2026-07-08 21:33:59 +0530

    Time: 1953 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,6 @@
+select contest_id, round(
+    count(distinct user_id)*100.0 /(select count(*) from users),2
+) as percentage
+from Register
+group by contest_id
+order by percentage desc, contest_id asc;
