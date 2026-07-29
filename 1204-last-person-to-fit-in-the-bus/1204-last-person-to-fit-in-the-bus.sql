08a09c3b6d606506b6352dd64e78458c9eea04c0
Bunty Saini <sainibunty2309@gmail.com>
2026-07-29 21:03:49 +0530

    Time: 853 ms (54.58%), Space: 0B (100%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+with cte as (
+    select person_name,
+           turn,
+           sum(weight) over (order by turn) as total_weight
+           from Queue
+)
+
+select person_name
+from cte
+where total_weight <= 1000
+order by total_weight desc
+limit 1;
